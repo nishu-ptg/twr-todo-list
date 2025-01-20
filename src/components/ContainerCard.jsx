807ae721b3f9355c815d38a45085ba6b551b9808
Nishu <nishuptg@yahoo.com>
2025-01-20 17:10:17 +0600

    Add ContainerCard component

---
+++ src/components/ContainerCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ContainerCard = () => {
+  return (
+    <div className="min-h-screen bg-none sm:bg-gradient-to-br sm:from-indigo-100  sm:to-indigo-50 flex justify-center items-center">
+      <div className="container mx-auto">
+        <div className="bg-white p-8 sm:rounded-lg sm:shadow-lg min-h-screen sm:min-h-[96vh] my-0 sm:my-[1.5vh]">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContainerCard;
